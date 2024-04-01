d10917ce8cf7c6b9b08d5cfad85da4fab1d5d860
Anastasiya Vusik <nastyavusik13@gmail.com>
2024-04-01 11:01:35 +0300

    first commit

---
+++ src/main/java/com/example/kitchendesign/KitchenDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitchendesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kitchendesign/KitchenDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitchendesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
