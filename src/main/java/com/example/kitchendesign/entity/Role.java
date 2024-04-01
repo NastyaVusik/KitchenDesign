596c8e6626e88d594dc7284d3f15458f15902240
Anastasiya Vusik <nastyavusik13@gmail.com>
2024-04-01 14:04:07 +0300

    Packagies: entity, repository, controller - were created

---
+++ src/main/java/com/example/kitchendesign/entity/Role.java
@@ -0,0 +1,6 @@
+package com.example.kitchendesign.entity;
+
+public enum Role {
+USER, INSTALLER, COMPANY_MANAGER, COMPANY_ADMIN, SUPER_ADMIN
+
+}
---
+++ src/main/java/com/example/kitchendesign/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.kitchendesign.service;
+
+import com.example.kitchendesign.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
